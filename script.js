405381cc3f2560efed61814d3bf2af58a97de848
Harut Qanaqaryan <hqanaqaryan@gmail.com>
2021-03-19 09:15:47 +0300

    update function position mouse

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const posX = document.querySelector('.pos-x')
 const posY = document.querySelector('.pos-y')
 
 document.addEventListener('mousemove', (event) => {
-    posX.value = event.pageX
-    posY.value = event.pageY
+    posX.value = event.pageX + "px"
+    posY.value = event.pageY + "px"
 });
 
